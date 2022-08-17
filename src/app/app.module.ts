ff3a3755be99085adc8c2fc42c47a15f2549b8ec
Mateusz Łędzewicz <mat.ledzewicz@gmail.com>
2022-08-17 07:50:14 +0200

    feat: add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +7,12 @@ import { EmployeeListComponentModule } from './employees/ui/employee-list/employ
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, EmployeeListComponentModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    EmployeeListComponentModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
