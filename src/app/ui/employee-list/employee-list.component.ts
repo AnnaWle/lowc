9eafb6451be33f29d082066eab46e212781853df
Anna Wleklinska <awleklinska@gmail.com>
2022-12-31 01:06:26 +0100

    feat; employees list added

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,8 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
 
@@ -13,5 +10,8 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
+
+  constructor(private _httpClient: HttpClient) {
+  }
 }
